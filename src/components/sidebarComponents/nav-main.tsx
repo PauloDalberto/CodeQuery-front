b93abc3a00143bad230b467b3d54ed0808be11b8
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2025-04-18 14:11:15 -0300

    first commit

---
+++ src/components/sidebarComponents/nav-main.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import { ChevronRight, PlusCircleIcon, type LucideIcon } from "lucide-react"
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar"
+
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string
+    url: string
+    icon?: LucideIcon
+    isActive?: boolean
+    items?: {
+      title: string
+      url: string
+    }[]
+  }[]
+}) {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>Conversas</SidebarGroupLabel>
+      <SidebarMenuButton
+        tooltip="Quick Create"
+        className="min-w-8 mb-2 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
+      >
+        <PlusCircleIcon />
+        <span>Nova conversa</span>
+      </SidebarMenuButton>
+      <SidebarMenu>
+        {items.map((item) => (
+          <Collapsible
+            key={item.title}
+            asChild
+            defaultOpen={item.isActive}
+            className="group/collapsible"
+          >
+            <SidebarMenuItem>
+              <CollapsibleTrigger asChild>
+                <SidebarMenuButton tooltip={item.title}>
+                  {item.icon && <item.icon />}
+                  <span>{item.title}</span>
+                  <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                </SidebarMenuButton>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                <SidebarMenuSub>
+                  {item.items?.map((subItem) => (
+                    <SidebarMenuSubItem key={subItem.title}>
+                      <SidebarMenuSubButton asChild>
+                        <a href={subItem.url}>
+                          <span>{subItem.title}</span>
+                        </a>
+                      </SidebarMenuSubButton>
+                    </SidebarMenuSubItem>
+                  ))}
+                </SidebarMenuSub>
+              </CollapsibleContent>
+            </SidebarMenuItem>
+          </Collapsible>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  )
+}
---
+++ src/components/theme/ThemeSwitcher.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { useTheme } from "next-themes";
+import { Button } from "../ui/button";
+
+export default function ThemeSwitcher(){
+  const { setTheme } = useTheme()
+
+  return (
+    <div className="my-3">
+      <Button onClick={() => setTheme('light')}>Light</Button> 
+      or 
+      <Button onClick={() => setTheme('dark')}>Dark</Button>  
+    </div>
+)
+}
