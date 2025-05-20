fc937bd6dab5386a07126a35dc4b48c0574fe81e
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2025-05-20 11:08:23 -0300

    refac: remove login with github

--- src/components/loginComponents/login-form.tsx
+++ src/components/loginComponents/login-form.tsx
@@ -4,7 +4,6 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import GitHubSvg from "../../../public/github.svg";
 import { useContext, useState } from "react";
 import { loginApi } from "@/services/auth/auth";
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -67,15 +66,7 @@ export function LoginForm({ className, ...props }: React.ComponentProps<"form">)
         <Button type="submit" className="w-full">
           Login
         </Button>
-        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-          <span className="bg-background text-muted-foreground relative z-10 px-2">
-            Ou continue com
-          </span>
-        </div>
-        <Button variant="outline" className="w-full">
-          <GitHubSvg />
-          Login com GitHub
-        </Button>
+
       </div>
       <div className="text-center text-sm">
         Não tem conta?{" "}
